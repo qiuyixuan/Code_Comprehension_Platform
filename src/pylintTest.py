ea5becce063e78054cf92036fe16a37a8db4469b
nbeck415 <natalielunbeck@yahoo.com>
2022-03-31 12:10:02 -0400

    text input to file output for pylint connection

--- src/pylintTest.py
+++ src/pylintTest.py
@@ -3,12 +3,15 @@ import sys
 from io import StringIO
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
+from src.file_format import FileFormat
 
 class PylintTest:
     def __init__(self):
         self.pylint_output = StringIO()
         self.reporter = TextReporter(self.pylint_output)
+        self.formatter = FileFormat()
 
     def analyze(self, input):
-        Run(["connect-4_before"], reporter=self.reporter, do_exit=False)
+        file = self.formatter.string_to_file(input)
+        Run([file], reporter=self.reporter, do_exit=False)
         return self.pylint_output.getvalue()
